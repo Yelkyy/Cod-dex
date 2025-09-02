85a2c184a497fd7ae2864d320738089be3fad1d2
Yelkyy <lr.jmpoblete@mmdc.mcl.edu.ph>
2025-09-02 18:48:57 +0800

    my first exercise in Codédex

---
+++ 06-storage-box.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+
+        int livingRoom = 15;
+        int mastersBedrm = 45;
+        int bathRm = 10;
+
+        System.out.println("The Living Room box has " + livingRoom + " items.");
+        System.out.println("The Master Bedroom box has " + mastersBedrm + " items.");
+        System.out.println("The Bathroom has " + bathRm + " items.");
+        
+    }
+}
