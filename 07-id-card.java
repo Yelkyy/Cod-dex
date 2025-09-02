293665eb0514aef6af823a4dd08a9a10ed23cfec
Yelkyy <lr.jmpoblete@mmdc.mcl.edu.ph>
2025-09-02 18:49:45 +0800

    Add HelloWorld class to display user information

---
+++ 07-id-card.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Write your code here 💖
+
+        String name = "Yelkyy";
+        String state = "Philippines";
+        char gender = 'F';
+        int age = 26;
+        boolean organDonor = false;
+
+        System.out.println("Name: " + name);
+        System.out.println("State: " + state);
+        System.out.println("Gender: " + gender);
+        System.out.println("Age: " + age);
+        System.out.println("Organ Donor: " + organDonor);
+    }
+}
